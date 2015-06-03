fbadef76bc079f8be94cacb7f12164968b1f0d74
Kyle Mahan <kyle.mahan@gmail.com>
2015-06-02 23:12:56 -0700

    Implement basic property parsing

---
+++ README.md
@@ -0,0 +1,5 @@
+# mf2j
+
+In-development Java parser for microformats2
+
+Requirements: Java 1.5+
---
+++ src/main/java/com/kylewm/mf2j/JsonList.java
@@ -0,0 +1,46 @@
+package com.kylewm.mf2j;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class JsonList extends ArrayList<Object> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1280821270688138705L;
+
+	public JsonList() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public JsonList(Collection<? extends Object> c) {
+		super(c);
+		// TODO Auto-generated constructor stub
+	}
+
+	public JsonList(int initialCapacity) {
+		super(initialCapacity);
+		// TODO Auto-generated constructor stub
+	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder("[");
+		boolean first = true;
+		for (Object value : this) {
+			if (!first) { sb.append(",");}
+			first = false;
+			if (value instanceof String) {
+				sb.append("\"" + JsonDict.escapeString((String) value) + "\"");
+			}
+			else {
+				sb.append(value);
+			}
+		}
+		sb.append("]");
+		return sb.toString();
+	}
+
+	
+}
