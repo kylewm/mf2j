c347ebeb1ef3c34628894a9d5185bd73878f8e4b
Emma Kuo <emma.c.kuo@gmail.com>
2016-03-03 17:17:10 -0800

    get test framework working with hcard

--- src/test/java/com/kylewm/mf2j/Mf2ParserTest.java
+++ src/test/java/com/kylewm/mf2j/Mf2ParserTest.java
@@ -1,21 +1,43 @@
 package com.kylewm.mf2j;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static org.junit.Assert.*;
 
 public class Mf2ParserTest
 {
+    protected String LoadResource(String resourcePath) throws URISyntaxException, IOException
+    {
+        URL url = this.getClass().getResource(resourcePath);
+        Path path = Paths.get(url.toURI());
+        return new String(Files.readAllBytes(path), "UTF8");
+    }
+
+    protected void ParseSample(String prefix) throws IOException, URISyntaxException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        Mf2Parser parser = new Mf2Parser();
+        String html = LoadResource(prefix + ".html");
+        String json = LoadResource(prefix + ".json");
+        JsonDict parsed = parser.parse(html, new URI(""));
+        JsonDict expected = mapper.readValue(json, JsonDict.class);
+        if (expected.containsKey("rel-urls"))
+            expected.remove("rel-urls");
+        assertTrue(parsed.equals(expected));
+    }
+
     @Test
-    public void SmokeTest() {
-        Mf2Parser mf2Parser = new Mf2Parser();
-        try
-        {
-            mf2Parser.parse("", new URI(""));
-        } catch (URISyntaxException e)
-        {}
+    public void HCard() throws IOException, URISyntaxException
+    {
+        ParseSample("/microformats-v2/h-card/hcard");
     }
 }
